1c370c9d97bd701331a9a87e9eebebf74bf75fcc
José Pérez <jpm70a@gmail.com>
2025-08-17 11:33:15 +0200

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  base: "/ctflow/" // 👈 IMPORTANTE: aquí pones el nombre del repo
-});
